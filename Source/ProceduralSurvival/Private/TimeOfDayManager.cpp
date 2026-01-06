8e4884f9d463ad95ae9876cc5dca531746bb1e9f
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-01-06 14:01:36 -0500

    initial time of day system

--- Source/ProceduralSurvival/Private/TimeOfDayManager.cpp
+++ Source/ProceduralSurvival/Private/TimeOfDayManager.cpp
@@ -2,6 +2,9 @@
 
 
 #include "TimeOfDayManager.h"
+#include "Engine/DirectionalLight.h"
+#include "Components/LightComponent.h"
+#include "Math/UnrealMathUtility.h"
 
 // Sets default values
 ATimeOfDayManager::ATimeOfDayManager()
@@ -15,7 +18,7 @@ ATimeOfDayManager::ATimeOfDayManager()
 void ATimeOfDayManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	UpdateSunAndMoon();
 }
 
 // Called every frame
@@ -23,5 +26,56 @@ void ATimeOfDayManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (AdvanceTime)
+	{
+		const float HoursPerSecond = TimeScale / 60.0f;
+		TimeOfDayHours = FMath::Fmod(TimeOfDayHours + HoursPerSecond * DeltaTime, 24.0f);
+
+		if (TimeOfDayHours < 0.0f)
+		{
+			TimeOfDayHours += 24.0f;
+		}
+
+		UpdateSunAndMoon();
+	}
+}
+
+float ATimeOfDayManager::SmoothStep(float Edge0, float Edge1, float X)
+{
+	const float Time = FMath::Clamp((X - Edge0) / (Edge1 - Edge0), 0.0f, 1.0f);
+	return Time * Time * (3.0f - 2.0f * Time);
+}
+
+void ATimeOfDayManager::UpdateSunAndMoon()
+{
+	const float DayAlpha = FMath::Fmod(TimeOfDayHours / 24.0f, 1.0f);
+	const float SunPitch = DayAlpha * 360.0f - 90.0f;
+
+	const float Elevation01 = FMath::Clamp(FMath::Sin(FMath::DegreesToRadians(SunPitch)), 0.0f, 1.0f);
+	const float DayLightFactor = SmoothStep(0.0f, FMath::Sin(FMath::DegreesToRadians(TwilightDegrees)), Elevation01);
+	const float NightLightFactor = 1.0f - DayLightFactor;
+
+	if (SunLight)
+	{
+		SunLight->SetActorRotation(FRotator(SunPitch, SunYawDegrees, 0.0f));
+
+		if (ULightComponent* SunLightComponent = SunLight->GetLightComponent())
+		{
+			SunLightComponent->SetIntensity(SunMaxIntensityLux * DayLightFactor);
+			SunLightComponent->SetVisibility(DayLightFactor > KINDA_SMALL_NUMBER);
+		}
+	}
+
+	if (MoonLight)
+	{
+		const float MoonPitch = SunPitch + 180.0f;
+		MoonLight->SetActorRotation(FRotator(MoonPitch, SunYawDegrees, 0.0f));
+
+		if (ULightComponent* MoonLightComponent = MoonLight->GetLightComponent())
+		{
+			MoonLightComponent->SetIntensity(MoonMaxIntensityLux * NightLightFactor);
+			MoonLightComponent->SetVisibility(NightLightFactor > KINDA_SMALL_NUMBER);
+		}
+	}
 }
 
--- Source/ProceduralSurvival/Public/TimeOfDayManager.h
+++ Source/ProceduralSurvival/Public/TimeOfDayManager.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "TimeOfDayManager.generated.h"
 
+class ADirectionalLight;
+
 UCLASS()
 class PROCEDURALSURVIVAL_API ATimeOfDayManager : public AActor
 {
@@ -15,12 +17,49 @@ public:
 	// Sets default values for this actor's properties
 	ATimeOfDayManager();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// In-game time in hours (0.0 - 24.0)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay")
+	float TimeOfDayHours = 12.0f;
+
+	// How many in-game minutes pass per real-time second
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay")
+	float TimeScale = 10.0f;
+
+	// Bool to control whether time progresses
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay")
+	bool AdvanceTime = true;
+
+	// Directional light representing the sun
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Lights")
+	TObjectPtr<ADirectionalLight> SunLight;
+
+	// Directional light representing the moon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Lights")
+	TObjectPtr<ADirectionalLight> MoonLight;
+
+	// Appearance Tuning
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Appearance")
+	float SunMaxIntensityLux = 100000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Appearance")
+	float MoonMaxIntensityLux = 0.25f;
+
+	// Controls how soft the transition is around sunrise/sunset (in degrees)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Appearance")
+	float TwilightDegrees = 6.0f;
+
+	// Direction of sunrise on yaw axis
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeOfDay | Appearance")
+	float SunYawDegrees = 0.0f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+private:	
+	void UpdateSunAndMoon();
+	static float SmoothStep(float Edge0, float Edge1, float X);
 };
